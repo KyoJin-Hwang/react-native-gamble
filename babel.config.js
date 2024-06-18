39c066721964ec14fae352a502367f40ff84dc88
kokong1231 <zhzk1001@hanmail.net>
2024-06-18 13:53:48 +0900

    iCON Add

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		['@babel/plugin-transform-class-properties', { loose: true }],
+		['@babel/plugin-transform-private-methods', { loose: true }],
+		['@babel/plugin-transform-private-property-in-object', { loose: true }],
+		[
+			'module-resolver',
+			{
+				root: ['src'],
+				extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+				alias: {
+						'@': './src',
+						'@/assets': './src/assets',
+						'@/components': './src/components',
+						'@/containers': './src/containers',
+						'@/views': './src/views',
+					},
+			},
+		],
+	]
 };
--- src/containers/Finger/Timer.ts
+++ src/containers/Finger/Timer.ts
@@ -1,1 +1,35 @@
-//
+interface PROPS {
+  handArray: HANDT[];
+  timeoutRef: React.MutableRefObject<NodeJS.Timeout | null>;
+  isCount: number;
+  setIsFinal: React.Dispatch<React.SetStateAction<boolean>>;
+  setHandArray: React.Dispatch<React.SetStateAction<HANDT[]>>;
+}
+
+interface HANDT {
+  idx: number;
+  x: number;
+  y: number;
+}
+
+const TimingCatch = (props: PROPS) => {
+  if (props.handArray.length !== props.isCount && props.handArray.length >= 2) {
+    if (props.timeoutRef.current) {
+      clearTimeout(props.timeoutRef.current);
+    }
+
+    props.timeoutRef.current = setTimeout(() => {
+      const endPoint = props.handArray.filter((data, idx) => {
+        if (data.idx === Math.floor(Math.random() * idx)) {
+          props.setIsFinal(true);
+          return true;
+        } else {
+          return false;
+        }
+      });
+      props.setHandArray(endPoint);
+    }, 3000);
+  }
+};
+
+export default TimingCatch;
