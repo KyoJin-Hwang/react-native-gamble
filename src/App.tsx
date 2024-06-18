f080ff6526ed672fa5a653252aef9e06118ec21b
kokong1231 <zhzk1001@hanmail.net>
2024-06-18 11:52:55 +0900

    Path config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import FingerPage from './views/FingerPage/index';
+import FingerPage from './views/FingerPage';
 
 function App(): React.JSX.Element {
   return (
