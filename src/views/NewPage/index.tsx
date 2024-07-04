3cafea4ce64ebeceb8ce7afb6e0257ca27bc3a08
Owen <owen1127@naver.com>
2024-07-04 15:27:24 +0900

    feat : new page 생성

---
+++ src/views/NewPage/index.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const NewPage = () => {
+  return (
+    <View>
+      <Text>NewPage</Text>
+    </View>
+  );
+};
+
+export default NewPage;
+
+const styles = StyleSheet.create({});
